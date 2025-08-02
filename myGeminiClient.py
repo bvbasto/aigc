56264fd6ddeb2325827002da9cb10b3de7d24b74
bvbasto <bvbasto@outlook.com>
2025-08-02 21:29:48 +0000

    8/2/2025, 10:29:48 PM

--- myGeminiClient.py
+++ myGeminiClient.py
@@ -192,8 +192,8 @@ class myGeminiClient:
                 msg = "TextChunk number " + str(ii) + "; tokens: " + str(tks) + " time " + str(ts) + "s"
                 self.bbp(msg)
             #if ii % printat <= 4 :
-            if ii < 4 :
-                self.bbp("\t" + chunk.text)
+            if ii < 3 :
+                self.bbp("\t\t\t" + chunk.text)
             ii = ii + 1
         
         d2 = datetime.now()
@@ -329,44 +329,64 @@ location= sk["region"]
 llmmodel= sk["model"]
 
 bucket_name="fdld-poc2"
-object_name="ai1/rawdocs/Fidelidade_Auto_Liber3G_CG058_AU052_mar2024.pdf"
+
 logbbp = True
 
 
 
 gg = myGeminiClient(project_id,gemini_api_key,location,llmmodel)
 
+descDoc = """
+Todo o texto deve estar presente nos blocos partidos dentro do limite de páginas indicado.
+Retorna um objeto json com o formato {"totalDeBlocos":<int>,"items":<list>}
+a lista de itens é feita por items com o seguinte formato:
+{"DocName":<string>,"BlocoID":<int>,"BlocoMetadata":<string>,"BlocoContent":<string>,"BlocoType":<string>,"docPage_ini":<int>,"docPage_end":<int>,"ClausulaOuTopico":<string>}"}
+os blocos são apenas uma divisão por isso o texto deve ser retornado na totalidade em cada blocoContent. 
+a chunkMetadata é para ser usada como metadados em queries sobre vectores, o chunkType deve indicar se é texto, código, tabela ou imagem. 
+docPage_ini e docPage_end tem a pagina inicial e final do bloco do documento de onde o bloco foi retirado
+ClausulaOuTopico deve indicar o capitulo, a clausula e o nome da tabela quando houver, DocName, é o nome do documento.
+O retorno deve ser somente o json indicado acime e mais nenhum outro texto.
+"""
+
 ##### creating chunks
 
-doc,pgs = getGCP_Doc(bucket_name,object_name)
-cacheInfo = "Este documento é sobre regras e clausulas referentes a seguro automovel " + object_name
-descDoc = f"""
+object_nameL5="ai1/rawdocs/Fidelidade_Auto_Liber3G_CG058_AU052_mar2024.pdf"
+docL5,pgsL5 = getGCP_Doc(bucket_name,object_nameL5)
+cacheInfoL5 = "Este documento é sobre regras e clausulas referentes a seguro automovel " + object_nameL5
+descDocL5 = f"""
 É necessário guardar todo o documento pdf numa base de dados vectorizada. Para isso é preciso partir o documento em n blocos para serem criados embeddings
 Os blocos devem estar sempre alinhados com clausulas e uma clausula não deve ser partida por estar em paginas diferentes nem deve haver varias clausulas por bloco
 Quando o texto não disser respeito a clausulas ou for uma tabela deve estar separado em blocos lógicos de assuntos
 """
-descDoc += """
-Todo o texto deve estar presente nos blocos partidos dentro do limite de páginas indicado.
-Retorna um objeto json com o formato {"totalDeBlocos":<int>,"items":<list>}
-a lista de itens é feita por items com o seguinte formato:
-{"DocName":"Seguro Automíve","BlocoID":<int>,"BlocoMetadata":<string>,"BlocoContent":<string>,"BlocoType":<string>,"docPage_ini":<int>,"docPage_end":<int>,"ClausulaOuTopico":<string>}"}
-os blocos são apenas uma divisão por isso o texto deve ser retornado na totalidade em cada blocoContent. 
-a chunkMetadata é para ser usada como metadados em queries sobre vectores, o chunkType deve indicar se é texto, código, tabela ou imagem. 
-docPage_ini e docPage_end tem a pagina inicial e final do bloco do documento de onde o bloco foi retirado
-ClausulaOuTopico deve indicar o capitulo, a clausula e o nome da tabela quando houver
-"""
+descDocL5 += descDoc
+cache_name,resp_text = gg.createChunksPDFDoc(docL5,pgsL5,cacheInfoL5,descDocL5)
 
+object_nameSS="ai1/rawdocs/CG 28 - Multicare - Fidelidade_DL_Final_Hifenizada.pdf"
+docSS,pgsSS = getGCP_Doc(bucket_name,object_nameSS)
+cacheInfoSS = "Este documento é sobre regras e clausulas referentes a um seguro de saude " + object_nameSS
+descDocSS = f"""
+É necessário guardar todo o documento pdf numa base de dados vectorizada. Para isso é preciso partir o documento em n blocos para serem criados embeddings
+Os blocos devem estar sempre alinhados com clausulas e uma clausula não deve ser partida por estar em paginas diferentes nem deve haver varias clausulas por bloco.
+As clausulas estão definidas no indice da pagina 2.
+Quando o texto não disser respeito a clausulas ou for uma tabela deve estar separado em blocos lógicos de assuntos
+"""
+descDocSS += descDoc
+cache_name,resp_text = gg.createChunksPDFDoc(docSS,pgsSS,cacheInfoSS,descDocSS)
 
 
 # cn = gg.createChunksPDFDoc_LoadDoc(doc,cacheInfo)
 # fr = gg.createChunksPDFDoc_GetTextChunks(cn,1,10,descDoc)
 # jl = gg.createChunksPDFDoc_CreateJSON(fr)
 # save_JsonFile(resp_json[1],"chunks.json")
-cache_name,resp_text = gg.createChunksPDFDoc(doc,pgs,cacheInfo,descDoc)
+
 j_list = gg.createChunksPDFDoc_CreateJSON_Final(resp_text)
+
+save_JsonFile(j_list,"chunks.json")
+
 dataset_id = 'rag_dataset_eu'
 table_id = 'testFomCode'
 schema = [
+        bigquery.SchemaField("DocName", "STRING", mode="NULLABLE"),  
         bigquery.SchemaField("BlocoID", "INTEGER", mode="REQUIRED"),
         bigquery.SchemaField("BlocoMetadata", "STRING", mode="NULLABLE"),
         bigquery.SchemaField("BlocoContent", "STRING", mode="NULLABLE"),
@@ -388,10 +408,12 @@ embeddings_model="text-multilingual-embedding-002"   # text-embedding-005
 region_embeddings = "eu"
 
 gg.rag_config(dataset_id,table_id_embeddings,region_embeddings)
-question="Usa as tabelas para calcular, tendo seguro faz 3 anos, qual o agravamento se tiver tido 2 sinistros nos ultimos 5 anos mas nenhum nos ultimos 2 anos?"
+question="Determina a classe para o seguro automovel, tendo 3 anos de seguro, qual o agravamento se tiver tido 2 sinistros nos ultimos 5 anos mas nenhum nos ultimos 2 anos?"
+question="Como pode ser resolvido o contrato de seguro de saude?"
 llm_answer,source_documents = gg.rag_question(question)
 llm_answer
 
+#Determina a classe para o seguro automovel, tendo 3 anos de seguro, qual o agravamento se tiver tido 2 sinistros nos ultimos 5 anos mas nenhum nos ultimos 2 anos?
 
 
 
@@ -399,10 +421,10 @@ llm_answer
 
 gg = myGeminiClient(project_id,gemini_api_key,location,llmmodel)
 
-doc,pgs = getGCP_Doc(bucket_name,object_name)
-cacheInfo = "Este documento é sobre regras e clausulas referentes a seguro automovel " + object_name
+doc,pgs = getGCP_Doc(bucket_name,object_nameL5)
+cacheInfo = "Este documento é sobre regras e clausulas referentes a seguro automovel " + object_nameL5
 cn = gg.createChunksPDFDoc_LoadDoc(doc,cacheInfo)
-question="Usa as tabelas para calcular, tendo seguro faz 3 anos, qual o agravamento se tiver tido 2 sinistros nos ultimos 5 anos mas nenhum nos ultimos 2 anos?"
+question="Determina a classe para o seguro automovel, tendo 3 anos de seguro, qual o agravamento se tiver tido 2 sinistros nos ultimos 5 anos mas nenhum nos ultimos 2 anos?"
 resp = gg.resp_using_cache(question,cn)
 resp
 
